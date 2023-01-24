55061cc1114002e3af22d6ff5db1c0914cf2eb17
SKYE KAERICHER <skyekaericher@gmail.com>
2023-01-24 11:53:41 -0600

    My first version of my aquarium code

---
+++ database.js
@@ -0,0 +1,25 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ 
+ * What is the data type of the database variable?
+What is the data type of the fish property?
+What is the data type of the name property?
+What is the data type of the size property?
+What data types will the fish array contain?
+
+
+ */
+const database = {
+    fish: [
+        {
+            species:
+            location:
+            name: "Bart",
+            diet: "crustaceans",
+            length: 3
+        }
+    ]
+}
